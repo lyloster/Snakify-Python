c442253007577aecd84f576e22678c9abe9730f5
Kristina Ilyovska <26176057+lyloster@users.noreply.github.com>
2020-01-13 19:37:53 -0500

    Find sum of factorials

--- 4/factorialSum.py
+++ 4/factorialSum.py
@@ -1,5 +1,3 @@
-#NOTE! Off by one
-
 # Given an integer n, print the sum 1!+2!+3!+...+n!.
 # This problem has a solution with only one loop,
 # so try to discover it. And don't use the math library :)
@@ -7,15 +5,15 @@
 def sum():
 	n = int(input())
 	sum = 0
-	for num in range (n+1):
+	for num in range (1, n+1):
 		sum += factorial(num)
 	print(sum)
 
 def factorial(n):
 	factorial = 1
-	for num in range(n, 0, -1):
+	for num in range(1, n+1):
 		# print("factorial ", factorial, " num ", num)
-		factorial = factorial * num 
+		factorial *= num 
 	return factorial
 
 
